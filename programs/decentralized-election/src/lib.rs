66f1c12e19be3c0581c519558dd50a6de45e9148
Rohit <RohitSinghEmail@protonmail.com>
2022-11-18 13:19:59 +0530

    election account and election initiation

--- programs/decentralized-election/src/lib.rs
+++ programs/decentralized-election/src/lib.rs
@@ -6,10 +6,52 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod decentralized_election {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<InitiateElection>, winners_count: u8) -> Result<()> {
+        require!(winners_count > 0, ElectionError::WinnerCountNotAllowed);
+
+        let election_account = &mut ctx.accounts.election_account;
+        election_account.candidate = 0;
+        election_account.phase = ElectionPhase::Registration;
+        election_account.election_officer = ctx.accounts.signer.key();
+        election_account.winners_count = winners_count;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(winners_count:u8)]
+pub struct InitiateElection<'info> {
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + 8 + 2 + 32 + 1 + 2 * (4 + winners_count as usize * 8)
+        )]
+    pub election_account: Account<'info, ElectionAccount>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct ElectionAccount {
+    pub candidate: u64,
+    pub phase: ElectionPhase,
+    pub election_officer: Pubkey,
+    pub winners_count: u8,
+    pub winners_id: Vec<u64>,
+    pub winners_votes_count: Vec<u64>,
+}
+
+#[derive(AnchorDeserialize, AnchorSerialize, PartialEq, Eq, Clone)]
+pub enum ElectionPhase {
+    Registration,
+    Voting,
+    Closed,
+}
+
+#[error_code]
+pub enum ElectionError {
+    WinnerCountNotAllowed,
+}
